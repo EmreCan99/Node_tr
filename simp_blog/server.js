d9e02a10aa5ee30358de7a544a9b7459ba6ca97d
emre <emrecanciftci99@gmail.com>
2025-11-05 19:24:13 +0100

    postgre con setup

--- simp_blog/server.js
+++ simp_blog/server.js
@@ -1,6 +1,31 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const PORT = 3000;
+
+// Database ------
+import pg from 'pg';
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "mydb",
+  password: "1234",
+  port: 5432
+})
+await db.connect();
+
+db.query('SELECT country FROM capitals LIMIT 5;', (err, res) => {
+  if(err){
+    console.log("Error during the SQL query; ", err.stack);
+  } else {
+    console.log("Query succesful");
+    console.log(res.rows);
+    countries = res.rows;
+    ;
+  }
+  db.end();
+})
+
+var countries = [];
 
 // Store posts in memory (will reset when server restarts)
 let posts = [];
@@ -20,6 +45,11 @@ app.get('/', (req, res) => {
 // New post page
 app.get('/new-post', (req, res) => {
   res.render('new-post');
+});
+
+// Quiz page
+app.get('/quiz', (req, res) => {
+  res.render('quiz-page', {country: "Türkiye"});
 });
 
 // Create a new post
