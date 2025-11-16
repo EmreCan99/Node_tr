8a1f3b48fcd453184e5b66c478e14d2dd561033f
emre <emrecanciftci99@gmail.com>
2025-11-16 19:36:47 +0100

    second intit clean

---
+++ react-app/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const date = new Date().getHours();
+  console.log(date);
+
+
+  return (
+    <>
+      <div className='body'>
+
+        <h1 >Hello</h1>
+        <ul>
+          <li>Yazı 1</li>
+          <li>Yazı 2</li>
+          <li>Yazı 3</li>
+        </ul>
+
+
+
+
+
+
+
+      </div>
+    </>
+  )
+}
+
+export default App;
